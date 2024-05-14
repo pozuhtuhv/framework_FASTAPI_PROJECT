1bef14feeeea21a0b5755e5463477cc9fe9fc22c
Hyeonseok-Cho <pozuhtuhv@gmail.com>
2024-05-14 11:54:49 +0900

    (update) ipblock_add

---
+++ 007_fastapi_ipblock/models.py
@@ -0,0 +1,27 @@
+# SQL 값에 대한 데이터 타입 설정
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+
+# 데이터베이스 테이블 매핑을 위한 모듈
+from database import Base
+
+
+# SQL 데이터베이스 Column, Datatype 설정
+class Users(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String(50), unique=True, index=True)
+    username = Column(String(50), unique=True, index=True)
+    role = Column(String(20))
+    hashed_password = Column(String(128))
+    auth_time = Column(String(30))
+
+
+class Posts(Base):
+    __tablename__ = "posts"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String(50))
+    description = Column(String(300))
+    username = Column(String(50))
+    post_time = Column(String(20))
--- README.md
+++ README.md
@@ -8,6 +8,16 @@
 ###### 004_fastapi_post_pagenation : Post Pagenation (페이징처리) 구축 
 ###### 005_fastapi_pydantic : pydantic 검증 연결 or dataclass 데이터 연결
 ###### 006_fastapi_basic_form : 모두 basic 기본폼으로 변경 read 추가 (페이지네이션 삭제 재구성중)
+###### 007_fastapi_ipblock : ipblock 기능 추가
+
+
+###### Auth 인증, post, Main 페이지 수정 (completed)
+###### Form 형식 제작 (login, post, edit) (completed)
+###### Admin API 작동 ( SQL 에서 role 권한 수정 -> 모든 유저 글 확인, 수정, 삭제 가능) (ing)
+###### Pydantic connect (completed) or dataclass way (completed)
+###### Post PageNation 작동 (ing)
+###### 기존 디자인보다 보기가 편한 클래식 디자인으로 변경 (completed)
+###### IPblock 차단 기능 추가 (SQL 연동 진행예정)
 
 ###### uvicorn main:app --reload --host=0.0.0.0 --port=80
 ###### https://www.yougetsignal.com/tools/open-ports/  -> 열린 포트 확인
